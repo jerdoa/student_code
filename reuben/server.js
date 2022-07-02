e3126d84fcb412e75e0ef768aae7a3bfa7e1f1f5
Reuben Nahouraii <Reuben.Nahouraii@gmail.com>
2022-07-02 11:36:24 -0700

    Note taking app (#13)
    * barebaones html, css, javascript
    
    * barebones server that writes

---
+++ reuben/server.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const fs = require('fs');
+const port = 3000;
+const filename = 'notes.txt'
+
+const server = http.createServer(async (request, response) => {
+    if (request.url == '/post/notes' && request.method == 'POST') {
+        const note = await getReqData(request);
+        fs.appendFile(filename, note + '\n', (error) => {
+            if (error) {
+                console.log('error occurred appending to file ', error)
+            }
+        });
+    response.write(note, ' note has been written');
+    response.end();
+    }
+});
+
+server.listen(port, (error) => {
+    if (error) {
+        console.log('a server error occured: ', error)
+    } else {
+        console.log('server listening on port ', port);
+    }
+});
+
+function getReqData(req) {
+    return new Promise((resolve, reject) => {
+        try {
+            let body = "";
+            // listen to data sent by client
+            req.on("data", (chunk) => {
+                // append the string version to the body
+                body += chunk.toString();
+            });
+            // listen till the end
+            req.on("end", () => {
+                // send back the data
+                resolve(body);
+            });
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
