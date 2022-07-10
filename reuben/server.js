105d2325f8577746b4ab92e032e895634740af06
Reuben Nahouraii <Reuben.Nahouraii@gmail.com>
2022-07-10 13:09:41 -0700

    Note taking app (#18)
    * barebones html, css, javascript
    
    * barebones server that writes
    
    * comment out code that writes to file

--- reuben/server.js
+++ reuben/server.js
@@ -1,19 +1,22 @@
 const http = require('http');
-const fs = require('fs');
+// const fs = require('fs');
 const port = 3000;
-const filename = 'notes.txt'
+// const filename = 'notes.txt'
 
 const server = http.createServer(async (request, response) => {
-    if (request.url == '/post/notes' && request.method == 'POST') {
-        const note = await getReqData(request);
-        fs.appendFile(filename, note + '\n', (error) => {
-            if (error) {
-                console.log('error occurred appending to file ', error)
-            }
-        });
-    response.write(note, ' note has been written');
+    // if (request.url == '/post/notes' && request.method == 'POST') {
+    //     // const note = await getReqData(request);
+    //     fs.appendFile(filename, note + '\n', (error) => {
+    //         if (error) {
+    //             console.log('error occurred appending to file ', error)
+    //         }
+    //     });
+    // response.write(note, ' note has been written');
+    // response.end();
+    // }
+
+    response.write('Hellllooo there!');
     response.end();
-    }
 });
 
 server.listen(port, (error) => {
@@ -24,22 +27,22 @@ server.listen(port, (error) => {
     }
 });
 
-function getReqData(req) {
-    return new Promise((resolve, reject) => {
-        try {
-            let body = "";
-            // listen to data sent by client
-            req.on("data", (chunk) => {
-                // append the string version to the body
-                body += chunk.toString();
-            });
-            // listen till the end
-            req.on("end", () => {
-                // send back the data
-                resolve(body);
-            });
-        } catch (error) {
-            reject(error);
-        }
-    });
-}
+// function getReqData(req) {
+//     return new Promise((resolve, reject) => {
+//         try {
+//             let body = "";
+//             // listen to data sent by client
+//             req.on("data", (chunk) => {
+//                 // append the string version to the body
+//                 body += chunk.toString();
+//             });
+//             // listen till the end
+//             req.on("end", () => {
+//                 // send back the data
+//                 resolve(body);
+//             });
+//         } catch (error) {
+//             reject(error);
+//         }
+//     });
+// }
